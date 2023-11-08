2aaae8d420ed5d871ef73347d6dc05dba14e1e74
LipinVV <vit.lipin@gmail.com>
2023-11-08 20:40:56 +0300

    Правки на основе замечаний к заданию по таймеру

--- 7-timer-notifications/index.js
+++ 7-timer-notifications/index.js
@@ -1,9 +1,10 @@
-const arguments = process.argv.slice(2)
-
 const notifier = require('node-notifier')
 const path = require("path")
 
+const arguments = process.argv.slice(2)
 const convertedArguments = arguments.map(argument => +argument.replace(/[^0-9]+/g, ""))
+
+const [hours,  minutes, seconds] = convertedArguments
 const currentLength = convertedArguments.length
 
 if(currentLength !== 3) {
@@ -11,29 +12,24 @@ if(currentLength !== 3) {
     process.exit(1)
 }
 
-const hours = convertedArguments.at(0)
-const minutes = convertedArguments.at(1)
-const seconds = convertedArguments.at(2)
-
-const hourValues = 3600
-const minuteValue = 60
+const hourConvertingValue  = 3600
+const minuteConvertingValue  = 60
 const convertingValue = 1000
 
-const secondsToHMS = seconds => {
+const totalCountdownInformer = seconds => {
     const convertedSeconds = seconds / convertingValue
 
-    const totalHours = Math.floor(convertedSeconds / hourValues);
-    const totalMinutes = Math.floor((convertedSeconds % hourValues) / minuteValue);
-    const totalSeconds = convertedSeconds % minuteValue;
+    const totalHours = Math.floor(convertedSeconds / hourConvertingValue)
+    const totalMinutes = Math.floor((convertedSeconds % hourConvertingValue) / minuteConvertingValue)
+    const totalSeconds = convertedSeconds % minuteConvertingValue
 
     // в дальнейшем можно добавить правильный выбор падежей
     console.log(`Таймер сработает через ${totalHours} часов ${totalMinutes} минут ${totalSeconds} секунд`)
 }
 
-
 if(convertedArguments.length > 0) {
-    const calculatedTimeGap = (hours * hourValues + minutes * minuteValue + seconds) * convertingValue
-    secondsToHMS(calculatedTimeGap)
+    const calculatedTimeGap = ((hours * hourConvertingValue) + (minutes * minuteConvertingValue) + seconds) * convertingValue
+    totalCountdownInformer(calculatedTimeGap)
 
     setTimeout(() => {
         notifier.notify({
@@ -45,4 +41,3 @@ if(convertedArguments.length > 0) {
         // имеются возможности для добавления реакций на разные события
     }, calculatedTimeGap)
 }
-
