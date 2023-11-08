8d55591376f3ab0f886870262f1bd69c701d046a
LipinVV <vit.lipin@gmail.com>
2023-11-08 20:27:34 +0300

    Правки по замечаниям

--- 4-timer/index.js
+++ 4-timer/index.js
@@ -1,6 +1,7 @@
 const arguments = process.argv.slice(2)
 
 const convertedArguments = arguments.map(argument => +argument.replace(/[^0-9]+/g, ""))
+const [hours,  minutes, seconds] = convertedArguments
 const currentLength = convertedArguments.length
 
 if(currentLength !== 3) {
@@ -8,20 +9,16 @@ if(currentLength !== 3) {
     process.exit(1)
 }
 
-const hours = convertedArguments.at(0)
-const minutes = convertedArguments.at(1)
-const seconds = convertedArguments.at(2)
-
-const hourValues = 3600
-const minuteValue = 60
+const hourConvertingValue  = 3600
+const minuteConvertingValue  = 60
 const convertingValue = 1000
 
-const secondsToHMS = seconds => {
+const totalCountdownInformer = seconds => {
     const convertedSeconds = seconds / convertingValue
 
-    const totalHours = Math.floor(convertedSeconds / hourValues);
-    const totalMinutes = Math.floor((convertedSeconds % hourValues) / minuteValue);
-    const totalSeconds = convertedSeconds % minuteValue;
+    const totalHours = Math.floor(convertedSeconds / hourConvertingValue)
+    const totalMinutes = Math.floor((convertedSeconds % hourConvertingValue) / minuteConvertingValue)
+    const totalSeconds = convertedSeconds % minuteConvertingValue
 
     // в дальнейшем можно добавить правильный выбор падежей
     console.log(`Таймер сработает через ${totalHours} часов ${totalMinutes} минут ${totalSeconds} секунд`)
@@ -29,8 +26,8 @@ const secondsToHMS = seconds => {
 
 
 if(convertedArguments.length > 0) {
-    const calculatedTimeGap = (hours * hourValues + minutes * minuteValue + seconds) * convertingValue
-    secondsToHMS(calculatedTimeGap)
+    const calculatedTimeGap = ((hours * hourConvertingValue) + (minutes * minuteConvertingValue) + seconds) * convertingValue
+    totalCountdownInformer(calculatedTimeGap)
 
     setTimeout(() => {
         console.log('Таймер сработал')
