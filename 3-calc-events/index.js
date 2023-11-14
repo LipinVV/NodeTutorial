377ad3639545b78f4204944db15677fd3fa6ecc7
LipinVV <vit.lipin@gmail.com>
2023-11-14 22:42:16 +0300

    Правки №2

--- 3-calc-events/index.js
+++ 3-calc-events/index.js
@@ -1,35 +1,29 @@
-const { EventEmitter } = require('events');
-const add = require('./operations/add')
-const multiply = require('./operations/multiply')
-const subtract = require('./operations/subtract')
-const divide = require('./operations/divide')
-
+const { EventEmitter } = require('events')
 const arguments = process.argv.slice(2)
 
 const calculationEmitter = new EventEmitter()
 
 const [firstValue, secondValue, operation] = arguments
 
-// то, что требовалось
+const operations = {
+    add: require('./operations/add'),
+    multiply: require('./operations/multiply'),
+    subtract: require('./operations/subtract'),
+    divide: require('./operations/divide'),
+}
+
 calculationEmitter.on('result', result => console.log(result))
 
-calculationEmitter.on('add', (firstValue, secondValue) => {
-    calculationEmitter.emit('result', add(firstValue, secondValue))
-})
-calculationEmitter.on('multiply', (firstValue, secondValue) => {
-    calculationEmitter.emit('result', multiply(firstValue, secondValue))
-})
-calculationEmitter.on('subtract', (firstValue, secondValue) => {
-    calculationEmitter.emit('result', subtract(firstValue, secondValue))
-})
-calculationEmitter.on('divide', (firstValue, secondValue) => {
-    calculationEmitter.emit('result', divide(firstValue, secondValue))
+Object.keys(operations).map(operation => {
+    calculationEmitter.on(operation, (firstValue, secondValue) => {
+        calculationEmitter.emit('result', operations[operation](firstValue, secondValue))
+    })
 })
 
 calculationEmitter.emit(operation, firstValue, secondValue)
 
 
-// то, как я это вижу:
+// мой начальный вариант:
 
 // const opsDirectory = 'operations'
 // const operationNames = ['add', 'multiply', 'subtract', 'divide']
